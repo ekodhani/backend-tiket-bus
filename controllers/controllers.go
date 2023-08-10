7187fc8457225985a6bfc7e584addbc0c3b36819
ekodhani <ekonurramadhani@gmail.com>
2023-08-10 21:39:17 +0700

    add function Get Bus

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -8,6 +8,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -131,4 +132,71 @@ func GetKursi(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusOK, results)
+}
+
+func GetBus(c *gin.Context) {
+	// Inisialisasi Struct Data Json
+	var otw models.Otw
+	// Hook data Json yg dikirim dan cek jika error tampilkan kenapa
+	if err := c.ShouldBindJSON(&otw); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Println("tgl berangkat : ", otw.Tgl_berangkat)
+	// Koneksi Database
+	db, err := database.Connect()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer db.Close()
+
+	// Query
+	query := query.GetBus(strconv.Itoa(otw.Tgl_berangkat), otw.Asal, otw.Tujuan)
+	rows, err := db.Query(query)
+	// Jika Error quernya
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer rows.Close()
+
+	// Buat Struct
+	var results []models.DataBus
+
+	// Loop melalui setiap baris hasil dan tambahkan data ke dalam slice
+	for rows.Next() {
+
+		var dataBus models.DataBus
+
+		// Scan data dari baris ke variabel-variabel
+		err := rows.Scan(&dataBus.Id, &dataBus.Nama_bus, &dataBus.Nomor_polisi, &dataBus.Jumlah_kursi, &dataBus.Type, &dataBus.Harga, &dataBus.Status)
+		if err != nil {
+			fmt.Println("Error scanning row:", err.Error())
+			return
+		}
+
+		// Tambahkan peta ke dalam slice
+		results = append(results, dataBus)
+	}
+	// Debug
+	fmt.Println(results)
+
+	// Cek apakah ada kesalahan saat mengiterasi melalui hasil
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during iteration:", err.Error())
+		return
+	}
+
+	// Jika ada Tgl Pulangnya
+	// if len(otw.Tgl_pulang) != 0 {
+	// 	fmt.Println("tgl pulang : ", otw.Tgl_pulang)
+	// }
+
+	if len(results) > 0 { // jika true
+		c.IndentedJSON(http.StatusOK, results)
+	} else { // jika data tidak ada
+		c.IndentedJSON(http.StatusNotFound, gin.H{
+			"message": "data tidak di temukan",
+		})
+	}
 }
